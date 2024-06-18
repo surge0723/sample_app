f262ee0d1cde2aea5e70386c7223ccfaf840f861
surge0723 <170217595+surge0723@users.noreply.githuglobalb.com>
2024-06-18 13:53:40 +0000

    削除機能搭載

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,12 +2,10 @@ class ListsController < ApplicationController
   def new
     @list = List.new  
   end
+  
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
     redirect_to list_path(list.id)
   end
 
@@ -27,6 +25,11 @@ class ListsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)  
+  end
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
   end
   
   private
